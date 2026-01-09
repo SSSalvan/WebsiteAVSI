e84807b2a3cd0e2a4862aa83daca65d4f111ada5
SSSalvan <silvanusalvan41@gmail.com>
2026-01-09 09:15:03 +0700

    fix

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('#mobile-menu');
     const menuLinks = document.querySelector('.nav-menu');
     const allLinks = document.querySelectorAll('.nav-link, .btn-primary');
+    // Tambahkan ini di bawah kode toggle menu yang sudah ada
+    const navLinks = document.querySelectorAll('.nav-link, .nav-menu .btn');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            const menu = document.querySelector('#mobile-menu');
+            const menuLinks = document.querySelector('.nav-menu');
+            
+            menu.classList.remove('is-active');
+            menuLinks.classList.remove('active');
+        });
+    });
 
     // Toggle menu
     menu.addEventListener('click', () => {
